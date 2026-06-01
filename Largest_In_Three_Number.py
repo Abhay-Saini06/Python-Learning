fb5da6154e9553f795a346edf714de19085eabe9
Abhay-Saini-06 <siddsaini0906@gmail.com>
2026-06-01 11:14:04 +0530

    Updating

---
+++ Largest_In_Three_Number.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))  
+if (a >= b and a >= c):
+    print("Largest number is a =",a)
+elif (b >= a and b >= c):
+    print("Largest number is b =",b)
+else:
+    print("Largest number is c =",c)
