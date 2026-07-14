61c2230d01101d7dbdc8d3e63df89c918a956ad9
Abhay-Saini-06 <siddsaini0906@gmail.com>
2026-07-14 16:14:53 +0530

    Initial commit

---
+++ Normal Programs/Calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the value 1 :"))
+b = int(input("Enter the value 2 :"))
+c = input("Enter the operation :")
+if(c == "+"):
+    print("The addition is ",a+b)
+elif(c == "-"):
+    print("The subtraction is",a-b)
+elif(c == "/"):
+    print("The division is ",a/b)
+elif(c == "%"):
+    print("The remainder is ",a%b)
